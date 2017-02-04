8920e75382c74bb5e1bcfffd4c3eb1418fe6ae7a
transcend-all <transcendentalguy@gmail.com>
2017-02-03 20:10:03 -0500

    stopped at step 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
 
+  # resources :question
+
+  get 'question/index'
+
+  get 'question/new'
+
+  get 'question/create'
+
+  get 'question/show'
+
+  get 'question/edit'
+
+  get 'question/update'
+
+  get 'question/destroy'
+
   resources :posts
 
   # get "posts/index"
