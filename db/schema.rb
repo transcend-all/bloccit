409ab9561545539f53cebd201da95e76941c47bc
transcend-all <transcendentalguy@gmail.com>
2017-02-12 15:16:48 -0500

    checkpoint 24 complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209214914) do
+ActiveRecord::Schema.define(version: 20170212182008) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -69,6 +69,14 @@ ActiveRecord::Schema.define(version: 20170209214914) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
