35d989647ae67c2da9e80ddfd6c01bc3dfff4fc2
transcend-all <transcendentalguy@gmail.com>
2017-02-19 16:30:44 -0500

    done except for failing specs

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,12 +21,15 @@ topics = Topic.all
 
 50.times do
 
-  Post.create!(
+  post = Post.create!(
     user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
+
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1,1].sample, user: users.sample)}
 end
 
 puts "#{Post.count}"
@@ -81,3 +84,4 @@ puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Topic.count} topics created"
 puts"#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
