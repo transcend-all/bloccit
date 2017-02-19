149866ab3e28707e21d28a3cfd4f6eb6a53d4c63
transcend-all <transcendentalguy@gmail.com>
2017-02-18 21:30:15 -0500

    checkpoint 28 complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218173848) do
+ActiveRecord::Schema.define(version: 20170218225519) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20170218173848) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -61,10 +61,17 @@ puts "#{Comment.count}"
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'transcendentalguy@gmail.com',
-  password: "asdfzxcv"
+admin = User.create!(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+)
+
+member = User.create!(
+  name: 'Member User',
+  email: 'member@example.com',
+  password: 'helloworld'
 )
 
 puts "Seed finished"
