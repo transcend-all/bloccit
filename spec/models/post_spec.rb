cd4f11f139671090372df0761a505ba636a1f9fb
transcend-all <transcendentalguy@gmail.com>
2017-02-22 13:18:50 -0500

    finished except for same failing spec problem from checkpoint

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -73,6 +73,21 @@ RSpec.describe Post, type: :model do
       end
     end
 
+    describe "#create_vote" do
+      it "sets the post up_votes to 1" do
+        expect(post.up_votes).to eq(1)
+      end
+    end
+
+    it "calls #create_vote when a post is created" do
+      post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+      expect(post).to receive(:create_vote)
+    end
+
+    it "associates the vote with the owner of the post" do
+      expect(post.votes.first.user).to eq(post.user)
+    end
+
     describe "#update_rank" do
       it "calculates the corrent rank" do
         post.update_rank
