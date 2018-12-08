3b5c19eecf5e9d0ae4b1e7608b449fabf7d9fb81
Evan <transcendentalguy@gmail.com>
2018-12-08 15:01:14 -0500

    updated dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -12,22 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170219221641) do
 
-  create_table "advertisements", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "answers", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "question_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["question_id"], name: "index_answers_on_question_id"
-  end
-
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "post_id"
@@ -57,14 +41,6 @@ ActiveRecord::Schema.define(version: 20170219221641) do
     t.float    "rank"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "sponsored_posts", force: :cascade do |t|
